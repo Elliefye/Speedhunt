c41d12d4d2e74c34572828e3d497f3f4057069c1
mi5kas <el.miskauskas@gmail.com>
2020-02-12 15:23:45 +0200

    something

--- Need for Seed/Assets/_scripts/iconFollowTrigger.cs
+++ Need for Seed/Assets/_scripts/iconFollowTrigger.cs
@@ -5,25 +5,26 @@ using UnityEngine.UI;
 
 public class iconFollowTrigger : MonoBehaviour
 {
-    public int roomNr = 0;
-    public GameObject tracker;
-    public Image icon;
-    public GameObject player;
-    public bool ableToGetActivated = true;
-    public float opaqueDistance = 10;
-    public float distance;
+    [SerializeField] Image template;
+    [SerializeField] Transform player;
+    [SerializeField] float opaqueDistance = 10;
+    Image iconClone;
 
+    void Start()
+    {
+        iconClone = Instantiate(template, Vector3.zero, Quaternion.zero).GetComponent<Image>();
+    }
     void Update()
     {
         distance = Vector3.Distance(this.transform.position, player.transform.position);
         Vector3 visTest = Camera.main.WorldToViewportPoint(this.transform.position);
-        if (ableToGetActivated && (visTest.x >= 0 && visTest.y >= 0) && (visTest.x <= 1 && visTest.y <= 1) && visTest.z >= 0)
+        if ((visTest.x >= 0 && visTest.y >= 0) && (visTest.x <= 1 && visTest.y <= 1) && visTest.z >= 0)
         {
             Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
-            icon.transform.position = namePose;
-            if (distance < opaqueDistance)
+            iconClone.transform.position = namePose;
+            if (distance < 10f)
             {
-                icon.color = new Color32(255, 255, 255, (byte)(255 - 255*(distance/opaqueDistance)));
+                iconClone.color = new Color32(255, 255, 255, (byte)(255 - 255*(distance/opaqueDistance)));
                 //icon.rectTransform.sizeDelta = new Vector2(70 / distance, 70 / distance);
                 //icon.color = new Color32(255, 255, 255, 255);
                 //icon.gameObject.SetActive(true);
@@ -33,15 +34,9 @@ public class iconFollowTrigger : MonoBehaviour
         }
         else
             icon.color = new Color32(255, 255, 255, 0);
+    }
+    void OnMouseDown()
+    {
 
-        if(tracker != null)
-        {
-            if (roomNr != tracker.GetComponent<roomNr>().currentRoom)
-            {
-                ableToGetActivated = false;
-                icon.gameObject.SetActive(false);
-            }
-            else ableToGetActivated = true;
-        }
     }
 }
